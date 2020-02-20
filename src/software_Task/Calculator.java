a1a5f81ebd8f4a9d2bf66bdd2ae966901502d2ba
Raghad <Raghaddmufeed@hotmail.com>
2020-02-20 21:06:37 +0200

    first commit

---
+++ src/software_Task/Calculator.java
@@ -0,0 +1,12 @@
+package software_Task;
+
+public class Calculator {
+    public int add(int first_number,int second_number) {
+    	return first_number+second_number;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
